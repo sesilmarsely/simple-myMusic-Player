342bd1cc33505a7151ff773b17d6abd62380c8ee
wahyujus <wahyukharismaz@gmail.com>
2020-05-31 21:23:47 +0700

    add existing file

---
+++ myMusic/myMusic/ViewController.swift
@@ -0,0 +1,118 @@
+//
+//  ViewController.swift
+//  myMusic
+//
+//  Created by sesilmarsely on 31/05/20.
+//  Copyright © 2020 sesilmarsely. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet var table: UITableView!
+
+    var songs = [Song]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureSongs()
+        table.delegate = self
+        table.dataSource = self
+    }
+
+    func configureSongs() {
+        songs.append(Song(name: "Gimme Love",
+                          albumName: "Single 2020",
+                          artistName: "Song by Joji",
+                          imageName: "cover1",
+                          trackName: "song1"))
+        songs.append(Song(name: "Zombie (데이식스)",
+                          albumName: "The Demon",
+                          artistName: "Day6",
+                          imageName: "cover2",
+                          trackName: "song2"))
+        songs.append(Song(name: "You Were Beautiful (예뻤어)",
+                          albumName: "Sunrise",
+                          artistName: "Day6",
+                          imageName: "cover3",
+                          trackName: "song3"))
+        songs.append(Song(name: "Gimme Love",
+                          albumName: "Single 2020",
+                          artistName: "Song by Joji",
+                          imageName: "cover1",
+                          trackName: "song1"))
+        songs.append(Song(name: "Zombie (데이식스)",
+                          albumName: "The Demon",
+                          artistName: "Day6",
+                          imageName: "cover2",
+                          trackName: "song2"))
+        songs.append(Song(name: "You Were Beautiful (예뻤어)",
+                          albumName: "Sunrise",
+                          artistName: "Day6",
+                          imageName: "cover3",
+                          trackName: "song3"))
+        songs.append(Song(name: "Gimme Love",
+                          albumName: "Single 2020",
+                          artistName: "Song by Joji",
+                          imageName: "cover1",
+                          trackName: "song1"))
+        songs.append(Song(name: "Zombie (데이식스)",
+                          albumName: "The Demon",
+                          artistName: "Day6",
+                          imageName: "cover2",
+                          trackName: "song2"))
+        songs.append(Song(name: "You Were Beautiful (예뻤어)",
+                          albumName: "Sunrise",
+                          artistName: "Day6",
+                          imageName: "cover3",
+                          trackName: "song3"))
+    }
+
+    // Table
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return songs.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let song = songs[indexPath.row]
+        // configure
+        cell.textLabel?.text = song.name
+        cell.detailTextLabel?.text = song.albumName
+        cell.accessoryType = .disclosureIndicator
+        cell.imageView?.image = UIImage(named: song.imageName)
+        cell.textLabel?.font = UIFont(name: "Helvetica-Bold", size: 18)
+        cell.detailTextLabel?.font = UIFont(name: "Helvetica", size: 17)
+
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+
+        // present the player
+        let position = indexPath.row
+        guard let vc = storyboard?.instantiateViewController(identifier: "player") as? PlayerViewController else {
+            return
+        }
+        vc.songs = songs
+        vc.position = position
+        present(vc, animated: true)
+    }
+
+
+}
+
+struct Song {
+    let name: String
+    let albumName: String
+    let artistName: String
+    let imageName: String
+    let trackName: String
+}
